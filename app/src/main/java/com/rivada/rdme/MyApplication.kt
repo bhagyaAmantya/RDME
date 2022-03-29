c5acfe58661e942ab79aab01e6e4df18281f76e9
BhagaNaik <bhagya.naik@amantyatech.com>
2022-03-29 17:21:20 +0530

    added logs.file in apk

--- app/src/main/java/com/rivada/rdme/MyApplication.kt
+++ app/src/main/java/com/rivada/rdme/MyApplication.kt
@@ -1,6 +1,15 @@
 package com.rivada.rdme
 
 import android.app.Application
+import android.util.Log
+import com.rivada.rdme.utils.FileLog.open
+import java.io.File
 
 
-class App: Application()
+class App:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val logFile = File(this.externalCacheDir!!.absolutePath, "/logs.txt")
+        open(logFile, Log.VERBOSE, 1000000)
+    }
+}
